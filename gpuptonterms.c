e17adcfbeb5349749669ef8caa47715b8bccb666
Bhavesh jain <151775274+Darkfalcon2804@users.noreply.github.com>
2023-12-27 11:29:51 +0530

    Update gpuptonterms.c

--- gpuptonterms.c
+++ gpuptonterms.c
@@ -1,14 +1,14 @@
 // ////Q)disply this gp -1,2,4,8,16,32 upto n terms????
 
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     printf("Enter number of terms:  ");
-//     scanf("%d",&n);
-//     int a=1;
-//     for(int i=1;i<=n;i++){
-//         printf("%d\n",a);
-//         a=a*2;
-//     }
-//     return 0 ;
-// }
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number of terms:  ");
+    scanf("%d",&n);
+    int a=1;
+    for(int i=1;i<=n;i++){
+        printf("%d\n",a);
+        a=a*2;
+    }
+    return 0 ;
+}
