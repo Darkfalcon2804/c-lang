795d3c1dd4bf837939c0dece6ef58e85f23d8fcf
Bhavesh jain <151775274+Darkfalcon2804@users.noreply.github.com>
2024-02-21 07:49:38 +0530

    Update maxoutofallelementsofarray.c

--- maxoutofallelementsofarray.c
+++ maxoutofallelementsofarray.c
@@ -5,8 +5,7 @@ int main(){
     int n;
     printf("Enter the size of array: ");
     scanf("%d",&n);
-    int arr[n];
-    int x=sizeof(arr)/sizeof(int);
+    int arr[n];
     printf("Enter the elements of array: ");
    for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
